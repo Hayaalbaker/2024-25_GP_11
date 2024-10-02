3eae67fc4853fb9aadd3f7a272b38dbbc190dc1c
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-10-02 13:56:38 +0300

    database

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_core/firebase_core.dart'; 
+import 'firebase_options.dart'; 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
