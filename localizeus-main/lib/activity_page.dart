53e94dc7e3506b895fb0dde212e09253d3acd675
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-10-28 15:29:37 +0300

    old files

--- localizeus-main/lib/activity_page.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ActivityPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Activity Page'));
-  }
-}
--- localizeus-main/lib/create_post_page.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CreatePostPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Create Post Page'));
-  }
-}
