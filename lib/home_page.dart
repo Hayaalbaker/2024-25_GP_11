13a4ba1601c19e6246429213520bd3a708386155
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-10-30 23:39:40 +0300

    nav in homepage works

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,40 +7,41 @@ import 'welcome_screen.dart'; // Import the welcome screen
 import 'package:firebase_auth/firebase_auth.dart';
 import 'places_widget.dart'; // Import the Places widget
 import 'profile_screen.dart'; // Import the profile screen
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Remove the debug banner here
+      home: HomePage(),
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   int _selectedIndex = 0; // Index to track the selected page
-  static const Color _iconColor =
-      Color.fromARGB(255, 218, 0, 0); // Constant color for icons
+  static const Color _iconColor = Color(0xFF800020); // Constant color for icons
+  late TabController _tabController; // TabController for managing tabs
 
-  // List of pages to show in the app
-  static List<Widget> _pages = <Widget>[
-    Center(child: Places_widget()), // Home page
-    SearchPage(), // Search Page
-    CreatePostPage(), // Create Post Page
-    ActivityPage(), // Activity Page
-    ProfileScreen(), // Profile Page
-  ];
-
-
-
-  // Titles for each page displayed in the AppBar
-  List<String> _titles = [
-    'Home',
-    'Search',
-    'Create Post',
-    'Notifications',
-    'Profile',
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this); // Initialize TabController
+  }
 
   // Method to change the selected page
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      _selectedIndex = index; // Update the selected index
     });
   }
 
@@ -79,8 +80,7 @@ class _HomePageState extends State<HomePage> {
               ),
               SizedBox(height: 20),
               ListTile(
-                leading: Icon(Icons.rate_review,
-                    color: const Color.fromARGB(255, 218, 0, 0)), // Set color
+                leading: Icon(Icons.rate_review, color: const Color(0xFF800020)), // Set color
                 title: Text('Post a Review'),
                 onTap: () {
                   Navigator.pop(context); // Close the modal
@@ -91,8 +91,7 @@ class _HomePageState extends State<HomePage> {
                 },
               ),
               ListTile(
-                leading: Icon(Icons.add_location_alt,
-                    color: const Color.fromARGB(236, 218, 0, 0)), // Set color
+                leading: Icon(Icons.add_location_alt, color: const Color(0xFF800020)), // Set color
                 title: Text('Add a Place'),
                 onTap: () {
                   Navigator.pop(context); // Close the modal
@@ -110,79 +109,88 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    _tabController.dispose(); // Dispose of the TabController
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:
-            Text(_titles[_selectedIndex]), // Set title based on selected page
-        automaticallyImplyLeading: false, // Remove default back button
-        actions: [
-          if (_selectedIndex == 0) // Show direct message icon on Home page only
-            IconButton(
-              icon: Icon(Icons.chat_bubble_outline,
-                  color: _iconColor), // Set color
-              onPressed: () {
-                // Action for direct messages
-              },
+    // Page options based on _selectedIndex
+    List<Widget> _pages = [
+      NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 300.0,
+              floating: false,
+              pinned: true,
+              stretch: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                collapseMode: CollapseMode.parallax,
+                background: Image.network(
+                  "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          IconButton(
-            icon: Icon(Icons.logout,
-                color: _iconColor), // Set color for sign-out icon
-            onPressed: _signOut, // Call the sign-out method
-            tooltip: 'Sign Out', // Tooltip for accessibility
-          ),
-        ],
+            SliverPersistentHeader(
+              delegate: _SliverAppBarDelegate(
+                TabBar(
+                  controller: _tabController,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  labelColor: Color(0xFF800020),
+                  unselectedLabelColor: Colors.grey,
+                  tabs: const [
+                    Tab(icon: Icon(Icons.rate_review), text: "Reviews"),
+                    Tab(icon: Icon(Icons.place), text: "Places"),
+                  ],
+                ),
+              ),
+              pinned: true,
+            ),
+          ];
+        },
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            Center(child: Text("Reviews Content", textAlign: TextAlign.center)), // Replace with actual reviews content
+            Places_widget(), // Display the Places widget here
+          ],
+        ),
       ),
-      body: _pages[_selectedIndex], // Display the selected page content
-      bottomNavigationBar: Container(
-        margin: EdgeInsets.only(
-            left: 10, right: 10, bottom: 10), // Margin for a floating effect
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30), // Rounded corners
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 10, // Shadow for floating effect
-              offset: Offset(0, 5),
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.transparent, // Transparent background
-          elevation: 0, // Remove default shadow
-          currentIndex: _selectedIndex, // Highlight the selected icon
-          onTap: _onItemTapped, // Change selected page on tap
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home,
-                    color: const Color.fromARGB(255, 184, 57, 57)),
-                label: ''), // Home
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search,
-                    color: const Color.fromARGB(255, 184, 57, 57)),
-                label: ''), // Search
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_box,
-                  color: const Color.fromARGB(
-                      255, 184, 57, 57)), // Create post icon
-              label: 'Post',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.notifications,
-                    color: const Color.fromARGB(255, 184, 57, 57)),
-                label: ''), // Activity
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person,
-                    color: const Color.fromARGB(255, 184, 57, 57)),
-                label: ''), // Profile
-          ],
-          selectedItemColor: const Color.fromARGB(
-              255, 184, 57, 57), // Set color for selected icon
-          unselectedItemColor: const Color.fromARGB(
-              255, 184, 57, 57), // Set color for unselected icons
-          type: BottomNavigationBarType.fixed,
-        ),
+      SearchPage(), // Navigate to Search Page
+      CreatePostPage(), // Navigate to Create Post Page
+      ActivityPage(), // Navigate to Activity Page
+      ProfileScreen(), // Navigate to Profile Page
+    ];
+
+    return Scaffold(
+      body: _pages[_selectedIndex], // Display the selected page based on the index
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex, // Highlight the selected icon
+        onTap: _onItemTapped, // Change selected page on tap
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: const Color(0xFF800020)),
+              label: ''), // Home
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: const Color(0xFF800020)),
+              label: ''), // Search
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box, color: const Color(0xFF800020)), // Create post icon
+              label: ''), // Create Post
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications, color: const Color(0xFF800020)),
+              label: ''), // Activity
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: const Color(0xFF800020)),
+              label: ''), // Profile
+        ],
+        selectedItemColor: const Color.fromARGB(255, 184, 57, 57),
+        unselectedItemColor: const Color.fromARGB(255, 184, 57, 57),
+        type: BottomNavigationBarType.fixed,
       ),
       floatingActionButton: GestureDetector(
         onTap: _onCreatePost, // Show modal on tapping the floating button
@@ -190,12 +198,31 @@ class _HomePageState extends State<HomePage> {
           elevation: 4.0,
           onPressed: _onCreatePost, // Show modal bottom sheet on press
           backgroundColor: _iconColor, // Set color for the floating button
-          child: Icon(Icons.add,
-              color: Colors.white), // Floating plus icon with white color
+          child: Icon(Icons.add, color: Colors.white), // Floating plus icon with white color
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation
-          .centerDocked, // Center the button above the nav bar
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, // Center the button above the nav bar
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return _tabBar;
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
--- lib/places_widget.dart
+++ lib/places_widget.dart
@@ -5,87 +5,47 @@ import 'view_Place.dart';
 class Places_widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // <1> Use StreamBuilder
-
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 230, 230, 230),
-          bottom: PreferredSize(
-            preferredSize: Size(200.0, 25.0),
-            child: SizedBox(
-              width: 200.0,
-              child: TabBar(
-                tabs: [
-                  Tab(
-                    icon: Icon(Icons.add_location_alt),
-                    text: "Places",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.rate_review),
-                    text: "Reviews",
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            PlacesWidget(),
-            Center(
-              child: Text("Reviews"),
-            ),
-          ],
-        ),
-      ),
-
-      // Use Expanded to take full height for the pages
+    return Scaffold(
+      body: PlacesList(), // Display the PlacesList widget
     );
   }
 }
 
-class PlacesWidget extends StatelessWidget {
+class PlacesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // <1> Use StreamBuilder
-    int itemCount = 6;
     return StreamBuilder<QuerySnapshot>(
-        // <2> Pass `Stream<QuerySnapshot>` to stream
-        stream: FirebaseFirestore.instance
-            .collection('places')
-            .orderBy('created_at', descending: true) // Ordering by timestamp
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            // <3> Retrieve `List<DocumentSnapshot>` from snapshot
-            final List<DocumentSnapshot> documents = snapshot.data!.docs;
-            return ListView(
-              children: documents
-                  .map((doc) => Card(
-                        child: ListTile(
-                          title: Text(doc['place_name']),
-                          subtitle: Text(doc['category']),
-                          onTap: () {
-                            // Use onTap instead of onPressed
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ViewPlace(
-                                    place_Id: doc.id), // Ensure the parameter name matches
-                              ),
-                            );
-                          },
-                        ),
-                      ))
-                  .toList(),
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return Text("Empty..");
-          }
-        });
+      stream: FirebaseFirestore.instance
+          .collection('places')
+          .orderBy('created_at', descending: true) // Ordering by timestamp
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final List<DocumentSnapshot> documents = snapshot.data!.docs;
+          return ListView(
+            children: documents.map((doc) => Card(
+              child: ListTile(
+                title: Text(doc['place_name']),
+                subtitle: Text(doc['category']),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ViewPlace(
+                        place_Id: doc.id, // Ensure the parameter name matches
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )).toList(),
+          );
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          return Center(child: Text("No places available."));
+        }
+      },
+    );
   }
 }
