ced2b4e34361075fce125ffaa57a2b4d53815bcd
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2025-05-06 15:48:48 +0300

    admin places updated with mariam's code

--- lib/places_widget.dart
+++ lib/places_widget.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:localize/main.dart';
 import 'bookmark_service.dart';
-import 'report_service.dart';
 import 'view_Place.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -14,7 +13,8 @@ class PlacesWidget extends StatelessWidget {
   final List<String>? placeIds;
   final String? filterCategory;
 
-  const PlacesWidget({Key? key, this.placeIds, this.filterCategory}) : super(key: key);
+  const PlacesWidget({Key? key, this.placeIds, this.filterCategory})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +22,10 @@ class PlacesWidget extends StatelessWidget {
       body: Center(
         child: Container(
           constraints: const BoxConstraints(maxWidth: 500),
-          child: PlacesList(placeIds: placeIds, filterCategory: filterCategory),
+          child: PlacesList(
+            placeIds: placeIds,
+            filterCategory: filterCategory,
+          ),
         ),
       ),
     );
@@ -33,7 +36,8 @@ class PlacesList extends StatefulWidget {
   final List<String>? placeIds;
   final String? filterCategory;
 
-  const PlacesList({Key? key, this.placeIds, this.filterCategory}) : super(key: key);
+  const PlacesList({Key? key, this.placeIds, this.filterCategory})
+      : super(key: key);
 
   @override
   _PlacesListState createState() => _PlacesListState();
@@ -44,8 +48,8 @@ class _PlacesListState extends State<PlacesList> {
   List<Map<String, dynamic>> recommendedPlaces = [];
   Timer? _debounce;
   int currentPage = 1;
-  bool hasMoreData = true;
-  int totalRecommendations = 0;
+  bool hasMoreData = true; // To track if there are more pages to load
+  int totalRecommendations = 0; // To store the total number of recommendations
 
   @override
   void initState() {
@@ -72,7 +76,10 @@ class _PlacesListState extends State<PlacesList> {
     if (user == null) return;
 
     try {
-      var userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+      var userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
       if (userDoc.exists && userDoc.data()!.containsKey('interests')) {
         setState(() {
           userInterests = List<String>.from(userDoc['interests'] ?? []);
@@ -103,7 +110,8 @@ class _PlacesListState extends State<PlacesList> {
 
         if (response.statusCode == 200) {
           final data = jsonDecode(response.body);
-          totalRecommendations = data['total'] ?? 0;
+          totalRecommendations =
+              data['total'] ?? 0; // Storing the total recommendations count
           debugPrint("📡 Server Data: ${response.body}");
           return List<Map<String, dynamic>>.from(data['recommendations'] ?? []);
         } else {
@@ -116,6 +124,7 @@ class _PlacesListState extends State<PlacesList> {
         await Future.delayed(const Duration(seconds: 1));
       }
     }
+
     return [];
   }
 
@@ -124,18 +133,24 @@ class _PlacesListState extends State<PlacesList> {
     _debounce = Timer(const Duration(milliseconds: 500), () async {
       try {
         debugPrint("⏳ Fetching recommended places from server...");
-        List<Map<String, dynamic>> recommendations = await sendUserIdToServer(page: page);
+        List<Map<String, dynamic>> recommendations =
+            await sendUserIdToServer(page: page);
 
         List<Map<String, dynamic>> filteredPlaces = [];
 
-        if (widget.filterCategory == null || widget.filterCategory == "All Categories") {
+        if (widget.filterCategory == null ||
+            widget.filterCategory == "All Categories") {
           filteredPlaces = recommendations;
         } else {
-          String selectedCategory = widget.filterCategory?.trim().toLowerCase() ?? "";
-          List<String> validCategories = selectedCategory.split(',').map((cat) => cat.trim()).toList();
+          String selectedCategory =
+              widget.filterCategory?.trim().toLowerCase() ?? "";
+          List<String> validCategories =
+              selectedCategory.split(',').map((cat) => cat.trim()).toList();
 
           for (var recommendation in recommendations) {
-            String placeCategory = (recommendation['category'] as String?)?.trim().toLowerCase() ?? "";
+            String placeCategory =
+                (recommendation['category'] as String?)?.trim().toLowerCase() ??
+                    "";
             if (validCategories.any((cat) => placeCategory.contains(cat))) {
               filteredPlaces.add(recommendation);
             }
@@ -150,101 +165,71 @@ class _PlacesListState extends State<PlacesList> {
               recommendedPlaces.addAll(filteredPlaces);
             }
 
-            if (filteredPlaces.isEmpty || recommendedPlaces.length >= totalRecommendations) {
+            if (filteredPlaces.isEmpty ||
+                recommendedPlaces.length >= totalRecommendations) {
               hasMoreData = false;
             }
           });
         }
 
         debugPrint("✅ Fetched ${filteredPlaces.length} places from page $page");
-        debugPrint("📊 Total shown: ${recommendedPlaces.length} / $totalRecommendations");
+        debugPrint(
+            "📊 Total shown: ${recommendedPlaces.length} / $totalRecommendations");
       } catch (e) {
         debugPrint("❌ Error fetching recommendations: $e");
       }
     });
   }
 
-  Query _getPlacesQuery() {
-    Query placesQuery = FirebaseFirestore.instance.collection('places');
-    if (widget.filterCategory != null && widget.filterCategory != "All Categories") {
-      placesQuery = placesQuery.where('category', isEqualTo: widget.filterCategory);
-    }
-    return placesQuery.orderBy('created_at', descending: true);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: recommendedPlaces.length,
-            itemBuilder: (context, index) {
-              var place = recommendedPlaces[index];
-              return _buildPlaceItem(
-                context,
-                place['id'] ?? 'unknown_id',
-                place['place_name'] ?? 'Unknown Place',
-                place['category'] ?? 'Unknown Category',
-                place['imageUrl'] ?? '',
-              );
-            },
-          ),
-        ),
-        if (hasMoreData)
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                currentPage++;
-                _fetchRecommendedPlaces(page: currentPage);
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: recommendedPlaces.length,
+              itemBuilder: (context, index) {
+                var place = recommendedPlaces[index];
+                return _buildPlaceItem(
+                  context,
+                  place['id'] ?? 'unknown_id',
+                  place['place_name'] ?? 'Unknown Place',
+                  place['category'] ?? 'Unknown Category',
+                  place['imageUrl'] ?? '',
+                );
               },
-              child: const Text("Load More"),
             ),
-          )
-        else
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text("No more results", style: TextStyle(color: Colors.grey)),
           ),
-        Expanded(
-          child: StreamBuilder<QuerySnapshot>(
-            stream: _getPlacesQuery().snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              if (snapshot.hasError) {
-                return Center(child: Text('❌ Error: ${snapshot.error}'));
-              }
-              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                return const Center(child: Text('⚠️ No available places.'));
-              }
-
-              final List<DocumentSnapshot> documents = snapshot.data!.docs;
-              return ListView.builder(
-                itemCount: documents.length,
-                itemBuilder: (context, index) {
-                  var doc = documents[index];
-                  if (!doc.exists) return const SizedBox();
-
-                  return _buildPlaceItem(
-                    context,
-                    doc.id,
-                    doc['place_name'] ?? 'Unknown Place',
-                    doc['category'] ?? 'Unknown Category',
-                    doc['imageUrl'] ?? '',
-                  );
+          if (hasMoreData)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  currentPage++;
+                  _fetchRecommendedPlaces(page: currentPage);
                 },
-              );
-            },
-          ),
-        ),
-      ],
+                child: const Text("Load More"),
+              ),
+            )
+          else
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child:
+                  Text("No more results", style: TextStyle(color: Colors.grey)),
+            ),
+        ],
+      ),
     );
   }
 
-  Widget _buildPlaceItem(BuildContext context, String placeId, String placeName, String category, String imageUrl) {
+  Widget _buildPlaceItem(
+    BuildContext context,
+    String placeId,
+    String placeName,
+    String category,
+    String imageUrl,
+  ) {
     if (placeId.isEmpty) return const SizedBox();
 
     return StreamBuilder<bool>(
@@ -275,7 +260,8 @@ class _PlacesListState extends State<PlacesList> {
                       image: DecorationImage(
                         image: imageUrl.isNotEmpty
                             ? CachedNetworkImageProvider(imageUrl)
-                            : const AssetImage('images/place_default_image.png') as ImageProvider,
+                            : const AssetImage('images/place_default_image.png')
+                                as ImageProvider,
                         fit: BoxFit.cover,
                       ),
                     ),
@@ -286,11 +272,17 @@ class _PlacesListState extends State<PlacesList> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(placeName, style: Theme.of(context).textTheme.titleLarge),
+                      Text(
+                        placeName,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
                       const SizedBox(height: 5),
                       Text(
                         category,
-                        style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.grey[600]),
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(color: Colors.grey[600]),
                       ),
                     ],
                   ),
@@ -303,20 +295,6 @@ class _PlacesListState extends State<PlacesList> {
                   onPressed: () async {
                     await BookmarkService().toggleBookmark(placeId, 'places');
                   },
-                ),
-                PopupMenuButton<String>(
-                  icon: Icon(Icons.more_vert, color: Colors.grey[700]),
-                  onSelected: (value) {
-                    if (value == 'report') {
-                      ReportService().navigateToReportScreen(context, placeId, 'Place');
-                    }
-                  },
-                  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                    const PopupMenuItem<String>(
-                      value: 'report',
-                      child: Text('Report'),
-                    ),
-                  ],
                 ),
               ],
             ),
